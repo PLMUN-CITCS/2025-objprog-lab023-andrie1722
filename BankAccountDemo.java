1b4524e55c2f05a918c7c1c9a3c159de6c338042
andrie flores <floreschristianandrie_bsit@plmun.edu.ph>
2025-03-02 21:52:11 +0800

    Add files via upload

---
+++ BankAccountDemo.java
@@ -0,0 +1,43 @@
+class BankAccount {
+    public String accountHolder;
+    private double balance;
+    protected String accountType;
+
+    // Constructor to initialize account attributes
+    public BankAccount(String accountHolder, double balance, String accountType) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
+
+    // Method to deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    // Method to withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    // Method to get the current balance
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class BankAccountDemo {
+    public static void main(String[] args) {
+        BankAccount myAccount = new BankAccount("Alice", 1000.0, "Checking");
+        myAccount.deposit(250.0);
+        myAccount.withdraw(100.0);
+
+        System.out.println("Account Holder: " + myAccount.accountHolder);
+        System.out.println("Account Type: " + myAccount.accountType);
+        System.out.println("Current Balance: $" + myAccount.getBalance());
+    }
+}
